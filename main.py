aaa47c39d27402c79d0562c1988ee07b885011e9
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2021-04-16 10:35:47 +0800

    Imported guides file, added humanise rig button

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from pymel.core import *
+from scripts/guides import *
 
 win = window(title="Awan's Auto Rigger")
 layout = columnLayout()
 
-#CREATE BUTTONS
+#BUTTONS
 
 guides_btn = button(l = "Create Guides", w = 200, p = layout)
 mirrorguides_btn = button(l = "Mirror Guides", w = 200, p = layout)
@@ -12,15 +13,19 @@ separator()
 displayaxes_btn = button(l = "Display Local Rotation Axes", w = 200, p = layout)
 separator()
 resetpose_btn = button(l = "Reset Pose", w = 200, p = layout)
+separator()
+humaniserig_btn = button(l = "Humanise Rig", w = 200, p = layout)
 deleterig_btn = button(l = "Delete Rig", w = 200, p = layout)
 separator()
-autorig_btn = button(l = "Awto Rig!", w = 200, p = layout)
+autorig_btn = button(l = "Awto Rig!", w = 200, h = 150, p = layout)
 
+#ADD COMMANDS TO BUTTONS WHEN PRESSED
 guides_btn.setCommand(createGuides)
 mirrorguides_btn.setCommand(mirrorGuides)
 deleterig_btn.setCommand(deleteGuides)
 displayaxes_btn.setCommand(displayAxes)
 resetpose_btn.setCommand(resetPose)
+humaniserig_btn.setCommand(humaniseRig)
 deleterig_btn.setCommand(deleteRig)
 autorig_btn.setCommand(createRig)
 
