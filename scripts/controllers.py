2f53b4aef382ffa3eb7b6f307f70b675fa5a1150
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2021-04-17 07:30:10 +0800

    Added controller class with create circle method

--- scripts/controllers.py
+++ scripts/controllers.py
@@ -36,3 +36,25 @@
 
 #Colour Overrides
 
+class Controller:
+
+	def __init__(self, name, parent):
+		self.name = name
+		self.parent = parent
+
+	def create(self, r = 10, nr = "Y"):
+		self.guide = ls(self.name + "_guide")
+		self.ctrl = MakeNurbCircle(r = r, n = self.name + "_ctrl")
+
+		if(nr == "X" || nr == "x"):
+			self.ctrl.setNormalX()
+		else if(nr == "Y" || nr == "y"):
+			self.ctrl.setNormalY()
+		else if(nr == "Z" || nr == "z"):
+			self.ctrl.setNormalZ()
+
+		self.ctrl_offset = MakeGroup(self.name + "_ctrl", n = self.name + "_ctrl_offset")
+		self.ctrl_offset.Transform().setTranslation(self.guide.getTranslation())
+		self.ctrl_offset.Transform().setRotation(self.guide.getRotation())
+		self.ctrl_offset.parent(self.parent + "_ctrl")
+
