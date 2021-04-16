1b1e5902ce56c690d9ee953caebe888efd8331ce
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2021-04-16 19:58:27 +0800

    initial commit

---
+++ scripts/controllers.py
@@ -0,0 +1,38 @@
+##CONTROLLERS##
+
+# SPINE #
+#Parent Constraint
+
+# HEAD #
+#Parent Constraint
+
+# ARMS FK #
+#Parent Constraint
+
+# ARMS IK #
+#Parent Constraint
+
+# FINGERS #
+#parent
+
+# LEGS FK #
+#Parent Constraint
+
+# LEGS IK #
+#Parent Constraint
+
+# FOOT IK #
+#Parent Constraint
+#Reverse Foot
+
+#IK FK Systems
+#IK FK Toggle
+
+#Pole Vector Constraint
+
+#Connections
+
+#Lock and Hide Attributes
+
+#Colour Overrides
+
